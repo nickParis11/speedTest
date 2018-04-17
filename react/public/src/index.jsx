8b3587e04d64dc957ca578bc43967b6ec951b9cd
Niklas <norr1000@gmail.com>
2018-04-17 21:43:51 +0200

    add simpliest react rendering

---
+++ react/public/src/index.jsx
@@ -0,0 +1,12 @@
+var log = console.log;
+
+log('i m alive');
+
+var react = require('react');
+var reactDOM = require('react-dom');
+
+
+ReactDOM.render(
+  <h1> Hello world </h1>,
+  document.getElementById('app')
+);
