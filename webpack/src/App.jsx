f15955614e4d27649216f5f378e6f969a5c16320
Niklas <norr1000@gmail.com>
2018-08-15 14:16:54 +0200

    add source file asset

---
+++ webpack/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () =>  {
+	return (
+		<h1> i should be App.js </h1>
+	);
+
+}
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+// test without retrun and curly bracks
+
---
+++ webpack/src/index.js
@@ -0,0 +1,7 @@
+console.log('i ve been required bu webpack');
+
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
+
+import App from './App';
